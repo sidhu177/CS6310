05c5a21da6200cfcb1309b6ce92f68195ec68bf1
sidhu177 <sidharth177@gmail.com>
2020-02-16 22:46:41 -0500

    added a java program

---
+++ NameDialog.java
@@ -0,0 +1,14 @@
+// Taken from Java How to Program by Paul Dietel 
+// This piece of code is to prompt user to enter name on a dialog box
+
+import javax.swing.JOptionPane;
+
+public class NameDialog
+{
+	public static void main(String[] args)
+	{
+		String name = JOptionPane.showInputDialog("What's your name ?");
+		String message = String.format("Welcome %s to the world", name);
+		JOptionPane.showMessageDialog(null, message);
+	}
+}
